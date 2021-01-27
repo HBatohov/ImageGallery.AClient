0cd19d55cedcc26f1e4e54bf7675fabf83226837
HBatohov <76105903+HBatohov@users.noreply.github.com>
2021-01-27 10:09:28 +0200

    Include bootstrap.

--- hb-gallery/src/app/app.module.ts
+++ hb-gallery/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ hb-gallery/src/app/models/room.ts
@@ -0,0 +1,5 @@
+export class Room{
+    id:any;
+    title:string = '';
+    description:string = '';
+}
